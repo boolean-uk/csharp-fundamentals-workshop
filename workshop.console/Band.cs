6f881d64fa57efac219b7b7b8e00f0df87114e2d
uerbzr <nigel.sibbert@gmail.com>
2025-08-08 09:28:25 +0100

    Added a Band search method

--- workshop.console/Band.cs
+++ workshop.console/Band.cs
@@ -19,5 +19,11 @@ namespace workshop.console
         public List<Person> Members { get { return _members; } }
         public int BandNetWorth { get { return _members.Sum(person => person.NetWorth); } }
         public string Name { get { return _name; } }
+        public List<Person> Search(string query)
+        {
+            List<Person> results = _members.Where(member => member.KnownAs.Contains(query, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return results;
+        }
     }
 }
